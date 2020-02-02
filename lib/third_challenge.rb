8affb0dbae70ec1f1f7c385da47ccba8054283cd
sasha walker <sashawalker31@gmail.com>
2020-02-02 13:23:59 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -10,6 +10,7 @@ def third_challenge
         #your key/value pairs here
         },
       :hero => {
+        name: "Romeo", age: "15", status: "alive"
         #your key/value pairs here
         },
       :hero_friends => []
